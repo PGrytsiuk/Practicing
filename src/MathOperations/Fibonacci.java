637ddbc22f22ba29d3fa5f49f6dd2dad20c8e296
PGrytsiuk <pgrytsiuk1992@gmail.com>
2024-09-21 01:29:33 +0300

    initial commit

---
+++ src/MathOperations/Fibonacci.java
@@ -0,0 +1,22 @@
+package MathOperations;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Fibonacci {
+    public static void main(String[] args) {
+
+        int n = 10, num1=0,  num2=1;
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Fibonacci series: " + " " + num1 +" "+  num2+" "  );
+
+        for (int i = 2; i < 10; i++) {
+            int num3=num1+num2;
+            num1=num2;
+            num2=num3;
+            System.out.printf(num3 +" ");
+        }
+
+    }
+    
+}
---
+++ src/StringOperations/PolindromTest.java
@@ -0,0 +1,17 @@
+package StringOperations;
+
+public class PolindromTest {
+    public static void main(String[] args) {
+        String str = "radar";
+        StringBuilder polindrom = new StringBuilder(str);
+       polindrom.reverse();
+
+
+        System.out.println(polindrom);
+        System.out.println(str.equals(polindrom.toString()));
+
+
+
+    }
+
+}
