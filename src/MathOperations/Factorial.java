a0948877768ef0ea8738b21285a07d2ad3dd7f16
PGrytsiuk <pgrytsiuk1992@gmail.com>
2024-09-21 13:50:08 +0300

    adding factorial

---
+++ src/MathOperations/Factorial.java
@@ -0,0 +1,15 @@
+package MathOperations;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        int n=5, factorial=1;
+
+        for(int i=1; i<=n; i++){
+            factorial*=i;
+        }
+        System.out.println(factorial);
+    }
+
+}
