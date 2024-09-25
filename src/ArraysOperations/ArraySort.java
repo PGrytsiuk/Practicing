6d436a808f17861684a39e2961aca63377451c1b
PGrytsiuk <pgrytsiuk1992@gmail.com>
2024-09-26 01:04:02 +0300

    ArraysSort

---
+++ src/ArraysOperations/ArraySort.java
@@ -0,0 +1,13 @@
+package ArraysOperations;
+
+
+import java.util.Arrays;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        int [] arr ={12, 5, 57, 7, 10, 11, 4, 8, 6};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
