9d9daf536d6a3eb9500baee091dbd57ca534e429
PGrytsiuk <pgrytsiuk1992@gmail.com>
2024-10-08 00:04:36 +0300

    CountDigits

---
+++ src/StringOperations/CountDigits.java
@@ -0,0 +1,14 @@
+package StringOperations;
+
+public class CountDigits {
+
+    public static void main(String[] args) {
+
+        int num = 123456789;
+        int count =  String.valueOf(num).length();
+
+        System.out.println(count);
+
+    }
+
+}
