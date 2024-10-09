0d3fc39b07947e45df70859f1da19ec01f07981e
PGrytsiuk <pgrytsiuk1992@gmail.com>
2024-10-09 16:16:32 +0300

    IntegerIntoBinary

--- src/MathOperations/ReverseNumber.java
+++ src/MathOperations/ReverseNumber.java
@@ -1,4 +1,19 @@
 package MathOperations;
 
 public class ReverseNumber {
+
+    public static void main(String[] args) {
+
+        int num = 12345;
+        int reversed = 0;
+        while(num != 0){
+
+            reversed = reversed * 10 + num % 10;
+            num /= 10;
+
+        }
+        System.out.println(reversed);
+
+    }
+
 }
