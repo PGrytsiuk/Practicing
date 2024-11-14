406b16d4f032dcafaaca7fd796a856704e11c4a7
Pavlo <33345335+PGrytsiuk@users.noreply.github.com>
2024-11-14 19:07:55 +0200

    Develop (#39)
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * GCD (#11)
    
    * Check for Anagram (#12)
    
    * PrimeNumUpdates
    
    * Updates (#15)
    
    * Remove intto2.0 (#7) (#8)
    
    * Develop (#10)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * Develop (#13)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * GCD (#11)
    
    * Check for Anagram (#12)
    
    * Werid commit
    
    * Develop (#14)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * GCD (#11)
    
    * Check for Anagram (#12)
    
    * PrimeNumUpdates
    
    * secondLargestArrayNum
    
    * swap two numbers (#17)
    
    * test Pascal triangle (#19)
    
    * Remove intto2.0 (#7) (#8)
    
    * Develop (#10)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * Develop (#13)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * GCD (#11)
    
    * Check for Anagram (#12)
    
    * Werid commit
    
    * Develop (#14)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * GCD (#11)
    
    * Check for Anagram (#12)
    
    * PrimeNumUpdates
    
    * secondLargestArrayNum
    
    * Develop (#16)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * GCD (#11)
    
    * Check for Anagram (#12)
    
    * PrimeNumUpdates
    
    * Updates (#15)
    
    * Remove intto2.0 (#7) (#8)
    
    * Develop (#10)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * Develop (#13)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * GCD (#11)
    
    * Check for Anagram (#12)
    
    * Werid commit
    
    * Develop (#14)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * GCD (#11)
    
    * Check for Anagram (#12)
    
    * PrimeNumUpdates
    
    * secondLargestArrayNum
    
    * Develop (#18)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * GCD (#11)
    
    * Check for Anagram (#12)
    
    * PrimeNumUpdates
    
    * Updates (#15)
    
    * Remove intto2.0 (#7) (#8)
    
    * Develop (#10)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * Develop (#13)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * GCD (#11)
    
    * Check for Anagram (#12)
    
    * Werid commit
    
    * Develop (#14)
    
    * Remove intto2.0 (#7)
    
    * Count doubles inside String (#9)
    
    * GCD (#11)
    
    * Check for Anagram (#12)
    
    * PrimeNumUpdates
    
    * secondLargestArrayNum
    
    * swap two numbers (#17)
    
    * Pascal triangle
    
    * Lambda expressions (#21)
    
    * practicing with arrays (#23)
    
    * countDuplicates (#25)
    
    * count spaces (#27)
    
    * new tasks (#29)
    
    * some practices 111024 (#31)
    
    * some practices refactoring 111024 (#33)
    
    * commit develop branch
    
    * commit develop branch EvenNumbers (#36)
    
    * commit develop branch RemoveWhiteSpaces and CommonElements (#38)

---
+++ src/StringOperations/RemoveWhiteSpaces.java
@@ -0,0 +1,17 @@
+package StringOperations;
+
+public class RemoveWhiteSpaces {
+
+    public static void main(String[] args) {
+
+        String test = "A u t o m a t i o n";
+        removeWhiteSpace(test);
+
+    }
+
+    public static void removeWhiteSpace(String test){
+        String result = test.replaceAll("\\s+", "");
+        System.out.println(result);
+
+    }
+}
