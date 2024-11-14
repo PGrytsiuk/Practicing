656cee862426a89b8c088a3baf9fe1eab93a6ee4
PGrytsiuk <pgrytsiuk1992@gmail.com>
2024-11-14 18:26:02 +0200

    commit develop branch EvenNumbers

---
+++ src/MathOperations/FindEvenNumbers.java
@@ -0,0 +1,27 @@
+package MathOperations;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class FindEvenNumbers {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        findEven(numbers);
+
+    }
+
+    public static void findEven(List<Integer> numbers){
+        // Define a predicate to check if a number is even
+        Predicate<Integer> isEven = number -> number  % 2 == 0;
+        // Use the predicate to filter the list
+        List<Integer> evenNumbers = numbers.stream()
+                                            .filter(isEven)
+                                            .collect(Collectors.toList());
+        System.out.println(evenNumbers);
+
+    }
+
+}
