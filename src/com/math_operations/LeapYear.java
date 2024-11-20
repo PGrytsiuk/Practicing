31c3b443c5be3d34f031d7f9efd64c985b7fa994
PGrytsiuk <pgrytsiuk1992@gmail.com>
2024-11-20 18:23:31 +0200

    leapYear v1

--- src/com/math_operations/LeapYear.java
+++ src/com/math_operations/LeapYear.java
@@ -1,6 +1,7 @@
 package com.math_operations;
 
 import java.text.ParseException;
+import java.util.function.Predicate;
 
 public class LeapYear {
 
@@ -9,11 +10,18 @@ public class LeapYear {
         int year = 2023;
 
         leapYearCheck(year);
+       boolean isLeap = checkLeapYearUsingPredicate(year);
+       System.out.println(isLeap);
 
     }
 
     public static void leapYearCheck(int year) {
         boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
                 System.out.println(isLeap);
+    }
+
+    public static boolean checkLeapYearUsingPredicate(int year) {
+        Predicate <Integer> isLeap = y -> (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0);
+        return isLeap.test(year);
     }
 }
