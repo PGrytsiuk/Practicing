53655b7d13860cf9a124832650b5760f6f386f5d
PGrytsiuk <pgrytsiuk1992@gmail.com>
2024-11-21 13:28:01 +0200

    added new practices v2

---
+++ src/com/string_operations/StringContains.java
@@ -0,0 +1,11 @@
+package com.string_operations;
+
+public class StringContains {
+
+    public static void main(String[] args) {
+
+        String str1 ="Test automation";
+        String str2 ="Test";
+        System.out.println(str1.contains(str2));
+    }
+}
