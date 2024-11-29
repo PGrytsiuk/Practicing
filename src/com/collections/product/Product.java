a6e4cd6cf0554cf7ab6c7d07a1bc52fce2172bd4
PGrytsiuk <pgrytsiuk1992@gmail.com>
2024-11-29 19:14:19 +0200

    Collections v2.0

--- src/com/collections/product/Product.java
+++ src/com/collections/product/Product.java
@@ -1,5 +1,7 @@
 package com.collections.product;
 
+import java.util.Comparator;
+
 public class Product {
      private String name;
      private int weight;
@@ -21,4 +23,15 @@ public class Product {
     public String toString(){
         return "Product{name='" + name + "', weight=" + weight + '}';
     }
+
+    public static final Comparator<Product> BY_WEIGHT =
+            Comparator.comparingInt(Product::getWeight);
+
+   /* public static final Comparator<Product> BY_WEIGHT = new Comparator<Product>() {
+        @Override
+        public int compare(final Product o1, final Product o2) {
+            return Integer.compare(o1.getWeight(), o2.getWeight());
+        }
+    } ;*/
+
 }
